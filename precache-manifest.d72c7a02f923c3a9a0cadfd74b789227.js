720c9f2e40fbbdf98d144c8f852869aa6c668123
nicolasariza <nicolas_ariza@outlook.com>
2020-04-12 20:46:31 -0500

    Updates

---
+++ precache-manifest.d72c7a02f923c3a9a0cadfd74b789227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76b78c4141caac0c219eb54299db91b",
+    "url": "/MERNTASKS-cliente/index.html"
+  },
+  {
+    "revision": "11b6e72354e162d9fd33",
+    "url": "/MERNTASKS-cliente/static/css/main.733a94b3.chunk.css"
+  },
+  {
+    "revision": "67741d51ffc2d6db5ab2",
+    "url": "/MERNTASKS-cliente/static/js/2.4a32e8c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MERNTASKS-cliente/static/js/2.4a32e8c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b6e72354e162d9fd33",
+    "url": "/MERNTASKS-cliente/static/js/main.59a7bb79.chunk.js"
+  },
+  {
+    "revision": "9c7f99cdde335fda871b",
+    "url": "/MERNTASKS-cliente/static/js/runtime-main.7ac214a1.js"
+  }
+]);
