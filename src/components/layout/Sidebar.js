5f22bcd71e2eedc4cac68a58fb5a4aee977061bf
nicolasariza <nicolas_ariza@outlook.com>
2021-04-10 16:41:07 -0500

    Sidebar fixed

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -5,14 +5,18 @@ import ListadoProyectos from '../proyectos/ListadoProyectos';
 const Sidebar = () => {
   return (
     <aside>
+      
       <h1>
         MERN<span>Tasks</span>
       </h1>
       <NuevoProyecto />
       <div className="proyectos">
         <h2>Tus proyectos</h2>
-        <ListadoProyectos />    
+        <div className="list-sidebar">
+          <ListadoProyectos />    
+        </div>
       </div>
+      <h3 className="footer-sidebar">Nicolas Ariza - 2020</h3>
     </aside>
   );
 };
